0bdd8ec7efca4db7b00d3d9d67b88245abf40062
anas-dev0 <anas.aouini@supcom.tn>
2025-04-30 17:00:14 +0100

    hackathon section

--- js/hackathon.js
+++ js/hackathon.js
@@ -1,47 +1,23 @@
-const descriptionPanel = document.querySelector('.description-panel');
-const widthp = window.innerWidth;
-gsap.registerPlugin(ScrollTrigger);
-    if(widthp<=768){
-    gsap.utils.toArray(".hackathon-panel.red").forEach((panel, i) => {
-        ScrollTrigger.create({
-            trigger: panel,
-            start: "top top",
-            endTrigger: ".hackathon-panel.last-panel",
-            end: "top bottom",
-            pin: true,
-            pinSpacing: false,
-        });
-    });
-    gsap.to(descriptionPanel, {
-        scrollTrigger: {
-            trigger: descriptionPanel,
-            start: "top top",
-            endTrigger: ".hackathon-panel.last-panel",
-            end: "top bottom",
-            pin: true,
-            pinSpacing: false,
-        },
-    });
-}
-if(widthp>768){
-    gsap.utils.toArray(".hackathon-panel:not(.description-panel)").forEach((panel, i) => {
-        ScrollTrigger.create({
-            trigger: panel,
-            start: "top top",
-            endTrigger: "#speaker-section",
-            end: "top bottom",
-            pin: true,
-            pinSpacing: false
-        });
-    });
-    const pinDescriptionPanel = gsap.to(descriptionPanel, {
-        scrollTrigger: {
-            trigger: descriptionPanel,
-            start: "top top",
-            endTrigger: "#speaker-section",
-            end: "top bottom",
-            pin: true,
-            pinSpacing: false,
-        },
-    });
-}
+// Countdown Timer
+const ahCountdown = () => {
+  const countDate = new Date("May 4, 2025 00:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+  
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+  
+  document.getElementById("ah-days").innerText = textDay < 10 ? `0${textDay}` : textDay;
+  document.getElementById("ah-hours").innerText = textHour < 10 ? `0${textHour}` : textHour;
+  document.getElementById("ah-minutes").innerText = textMinute < 10 ? `0${textMinute}` : textMinute;
+  document.getElementById("ah-seconds").innerText = textSecond < 10 ? `0${textSecond}` : textSecond;
+};
+
+setInterval(ahCountdown, 1000);
